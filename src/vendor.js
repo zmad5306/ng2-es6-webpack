93386ff1587a76d0982790de9043908181f0a47e
Zach Maddox <zachary.maddox@gmail.com>
2017-05-18 23:16:03 -0400

    stubbed out project

---
+++ src/vendor.js
@@ -0,0 +1,10 @@
+import './polyfill';
+import 'rxjs';
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/forms';
+import '@angular/http';
+import '@angular/router';
+import '@angular/core';
+import '@angular/common';
+import '@angular/compiler';
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+let webpack = require('webpack');
+let path = require('path');
+let CopyWebpackPlugin = require('copy-webpack-plugin');
+
+let context = path.join(process.cwd(), 'src');
+
+let plugins = [
+    new CopyWebpackPlugin([
+        { from: 'index.html' }
+    ])
+];
+
+let entry = {
+    'main': './main.js',
+    'vendor':'./vendor.js'
+};
+
+let output = {
+    path: path.resolve(process.cwd(), 'public'),
+    filename: '[name].bundle.js',
+    sourceMapFilename: '[name].map',
+    chunkFilename: '[id].chunk.js'
+};
+
+let resolve = {
+  modules: [
+      'node_modules', 
+      path.resolve(process.cwd(), 'src')
+  ],
+  extensions: ['.js', '.json']
+};
+
+let rules = [
+    {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules)/,
+        query: {
+            presets: ['latest', 'angular2']
+        }
+    },
+    {
+        test:/\.js$/,
+        use: 'source-map-loader',
+        exclude: [
+            path.join(process.cwd(), 'node_modules/rxjs'),
+            path.join(process.cwd(), 'node_modules/@angular')
+        ]
+    },
+    {
+        test: /\.html$/,
+        use: 'html-loader?attrs=false&caseSensitive&removeAttributeQuotes=false'
+    }
+];
+
+module.exports = {
+    context: context,
+    plugins: plugins,
+    resolve: resolve,
+    entry: entry,
+    output: output,
+    module: { rules: rules }
+};
